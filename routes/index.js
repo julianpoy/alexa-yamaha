0f89f0a6f8c9415b9d68730ffbbfc4e4972998b5
Julian Poyourow <julianpoyo@gmail.com>
2016-10-22 19:06:54 -0700

    Updated project with essentials

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var YamahaAPI = require('yamaha-nodejs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/action', function(req, res, next) {
+  
+  
+  res.status(200).send("ok");
 });
 
 module.exports = router;
